e06632b9b2a1627438b59dd299a6b58b13c15cd7
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-05-28 11:24:27 +0800

    feat: AUR-10 update db schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -11,45 +11,203 @@ import {
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
-export const Post = pgTable("post", {
+export const Workspace = pgTable("post", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
-  title: varchar("name", { length: 256 }).notNull(),
-  content: text("content").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updatedAt", {
+  name: varchar("name", { length: 256 }).notNull(),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updatedAt", {
+    mode: "date",
+    withTimezone: true,
+  }).$onUpdateFn(() => sql`now()`),
+  owner_id: uuid("owner_id")
+    .notNull()
+    .references(() => User.id),
+});
+
+export const workspaceRelations = relations(Workspace, ({ one }) => ({
+  owner: one(User, { fields: [Workspace.owner_id], references: [User.id] }),
+}));
+
+export const CreateWorkspaceSchema = createInsertSchema(Workspace, {
+  name: z.string().max(256),
+}).omit({
+  id: true,
+  created_at: true,
+  updated_at: true,
+});
+
+export const Work = pgTable("work", {
+  id: uuid("id").notNull().primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull(),
+  content: text("content"),
+  owner_id: uuid("owner_id")
+    .notNull()
+    .references(() => User.id),
+  workspace_id: uuid("workspace_id")
+    .notNull()
+    .references(() => Workspace.id),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updatedAt", {
     mode: "date",
     withTimezone: true,
   }).$onUpdateFn(() => sql`now()`),
 });
 
-export const CreatePostSchema = createInsertSchema(Post, {
-  title: z.string().max(256),
-  content: z.string().max(256),
-}).omit({
-  id: true,
-  createdAt: true,
-  updatedAt: true,
+export const workRelations = relations(Work, ({ one, many }) => ({
+  owner: one(User, { fields: [Work.owner_id], references: [User.id] }),
+  workspace: one(Workspace, {
+    fields: [Work.workspace_id],
+    references: [Workspace.id],
+  }),
+  workCharacter: many(WorkCharacter),
+}));
+
+export const Dialogue = pgTable("dialogue", {
+  id: uuid("id").notNull().primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull(),
+  content: text("content"),
+  start_at: integer("start_at").notNull(),
+  end_at: integer("end_at").notNull(),
+  owner_id: uuid("owner_id")
+    .notNull()
+    .references(() => User.id),
+  workspace_id: uuid("workspace_id")
+    .notNull()
+    .references(() => Workspace.id),
+  work_id: uuid("work_id")
+    .notNull()
+    .references(() => Work.id),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updatedAt", {
+    mode: "date",
+    withTimezone: true,
+  }).$onUpdateFn(() => sql`now()`),
 });
+
+export const dialogueRelations = relations(Dialogue, ({ one, many }) => ({
+  owner: one(User, { fields: [Dialogue.owner_id], references: [User.id] }),
+  workspace: one(Workspace, {
+    fields: [Dialogue.workspace_id],
+    references: [Workspace.id],
+  }),
+  work: one(Work, { fields: [Dialogue.work_id], references: [Work.id] }),
+  characterDialogue: many(CharacterDialogue),
+}));
+
+export const Character = pgTable("character", {
+  id: uuid("id").notNull().primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull(),
+  avatar: text("avatar"),
+  description: text("description"),
+  owner_id: uuid("owner_id")
+    .notNull()
+    .references(() => User.id),
+  workspace_id: uuid("workspace_id")
+    .notNull()
+    .references(() => Workspace.id),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updatedAt", {
+    mode: "date",
+    withTimezone: true,
+  }).$onUpdateFn(() => sql`now()`),
+});
+
+export const characterRelations = relations(Character, ({ one, many }) => ({
+  owner: one(User, { fields: [Character.owner_id], references: [User.id] }),
+  workspace: one(Workspace, {
+    fields: [Character.workspace_id],
+    references: [Workspace.id],
+  }),
+  workCharacter: many(WorkCharacter),
+  characterDialogue: many(CharacterDialogue),
+}));
+
+export const WorkCharacter = pgTable(
+  "work_character",
+  {
+    work_id: uuid("work_id")
+      .notNull()
+      .references(() => Work.id),
+    character_id: uuid("character_id")
+      .notNull()
+      .references(() => Character.id),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updatedAt", {
+      mode: "date",
+      withTimezone: true,
+    }).$onUpdateFn(() => sql`now()`),
+  },
+  (workCharacter) => ({
+    pk: primaryKey({
+      columns: [workCharacter.work_id, workCharacter.character_id],
+    }),
+  }),
+);
+
+export const WorkCharacterRelations = relations(WorkCharacter, ({ one }) => ({
+  work: one(Work, { fields: [WorkCharacter.work_id], references: [Work.id] }),
+  character: one(Character, {
+    fields: [WorkCharacter.character_id],
+    references: [Character.id],
+  }),
+}));
+
+export const CharacterDialogue = pgTable(
+  "character_dialogue",
+  {
+    character_id: uuid("character_id")
+      .notNull()
+      .references(() => Character.id),
+    dialogue_id: uuid("dialogue_id")
+      .notNull()
+      .references(() => Dialogue.id),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updatedAt", {
+      mode: "date",
+      withTimezone: true,
+    }).$onUpdateFn(() => sql`now()`),
+  },
+  (characterDialogue) => ({
+    pk: primaryKey({
+      columns: [characterDialogue.character_id, characterDialogue.dialogue_id],
+    }),
+  }),
+);
+
+export const CharacterDialogueRelations = relations(
+  CharacterDialogue,
+  ({ one }) => ({
+    character: one(Character, {
+      fields: [CharacterDialogue.character_id],
+      references: [Character.id],
+    }),
+    dialogue: one(Dialogue, {
+      fields: [CharacterDialogue.dialogue_id],
+      references: [Dialogue.id],
+    }),
+  }),
+);
 
 export const User = pgTable("user", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
   name: varchar("name", { length: 255 }),
+  avatar: text("avatar"),
   email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("emailVerified", {
+  email_verified: timestamp("emailVerified", {
     mode: "date",
     withTimezone: true,
   }),
-  image: varchar("image", { length: 255 }),
 });
 
-export const UserRelations = relations(User, ({ many }) => ({
+export const userRelations = relations(User, ({ many }) => ({
+  workspaces: many(Workspace),
   accounts: many(Account),
 }));
 
 export const Account = pgTable(
   "account",
   {
-    userId: uuid("userId")
+    user_id: uuid("user_id")
       .notNull()
       .references(() => User.id, { onDelete: "cascade" }),
     type: varchar("type", { length: 255 })
@@ -73,12 +231,12 @@ export const Account = pgTable(
 );
 
 export const AccountRelations = relations(Account, ({ one }) => ({
-  user: one(User, { fields: [Account.userId], references: [User.id] }),
+  user: one(User, { fields: [Account.user_id], references: [User.id] }),
 }));
 
 export const Session = pgTable("session", {
   sessionToken: varchar("sessionToken", { length: 255 }).notNull().primaryKey(),
-  userId: uuid("userId")
+  user_id: uuid("user_id")
     .notNull()
     .references(() => User.id, { onDelete: "cascade" }),
   expires: timestamp("expires", {
@@ -88,5 +246,5 @@ export const Session = pgTable("session", {
 });
 
 export const SessionRelations = relations(Session, ({ one }) => ({
-  user: one(User, { fields: [Session.userId], references: [User.id] }),
+  user: one(User, { fields: [Session.user_id], references: [User.id] }),
 }));
