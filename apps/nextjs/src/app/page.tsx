7eb6280c7fe52ea3bf74d620fafcf2e516ffcdf9
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-06-08 10:02:50 +0800

    feat: update RootPage

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,24 +1,13 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import type { MDXEditorMethods } from "@mdxeditor/editor";
-import * as React from "react";
-import dynamic from "next/dynamic";
+import { api } from "~/trpc/server";
 
-const Editor = dynamic(() => import("~/components/editor"), { ssr: false });
+export default async function RootPage() {
+  const workspaces = await api.workspace.all();
 
-export default function HomePage() {
-  const ref = React.useRef<MDXEditorMethods>(null);
-
-  return (
-    <div
-      onClick={() => {
-        ref.current?.focus();
-      }}
-      className="h-full w-full"
-    >
-      <div className="mx-auto h-full max-w-[var(--centralized-content-width)] py-[150px]">
-        <Editor editorRef={ref} markdown="## test" />
-      </div>
-    </div>
-  );
+  if (workspaces.length === 0 || !workspaces[0]) {
+    return redirect("/onboarding");
+  } else {
+    return redirect(`/${workspaces[0].id}`);
+  }
 }
